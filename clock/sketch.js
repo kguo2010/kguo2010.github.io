23c6d8c5b52b2dd7010181fe5c6e14546ffe3ad4
kguo <kguo2010@gmail.com>
2020-01-25 15:25:35 -0500

    Revert "removing unnecessary stuff"
    This reverts commit d8d01896393a64f56d217537bf4a6040a7be6267.

--- clock/sketch.js
+++ clock/sketch.js
@@ -9,16 +9,47 @@ function setup() {
 function draw() {
 	stroke('#222222')
 	background(10000);
-
-	
-	//code below
-
-
+	//second background
+	for (let i = 0; i < 60; i++) {
+		strokeWeight(3)
+		stroke(0,0,255,150)
+		line(1 + i * space, 500 - i * 4, 1 + i * space, 600);
+	}
+	//second hand
 	strokeWeight(3)
 	stroke(0,0,255,150)
-	line(second() * 2, 500, 0, 600);
+	line(second() * space, 500 - second() * 4, second() * space, 600);
 	//minute background
+	for (let i = 0; i < 60; i++) {
+		strokeWeight(5)
+		stroke(225,204,0,150)
+		line(20 + i * (space + 2), 0, 20 + i * (space  + 2), 400 - i * 6);
+	}
+	//minute progress bar
+	for (let i = 0; i < minute(); i++) {
+		strokeWeight(5)
+		stroke(825,104,0,150)
+		line(20 + i * (space + 2), 0, 20 + i * (space + 2), 400-i*6);
+	}
+	//minute hand
+	strokeWeight(3)
+	stroke('red')
+	line(20+minute()*(space+2), 0, 20+minute()*(space+2), 400-minute()*6);
+	//hour bars
+	for (let i = 0; i < hour(); i++) {
 
+		if (i % 2 == 0) {
+		stroke(100 + (i-1) * (space + 20),0 + (i-1) * (space + 5),255 + (i-1) * (space + 20),150)
+		strokeWeight(20)
+		line(400 + i * (space + 10), 0, 400 + i * (space + 10), 300);
+
+		}
+		else {
+		stroke(100 + i * (space + 20),0 + i * (space + 5),255 + i * (space + 20),150)
+		strokeWeight(20)
+		line(400 + (i-1) * (space + 10), 325, 400 + (i-1) * (space + 10), 600);
+		}
+	}
 }
 
 
